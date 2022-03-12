941982bc5b3ff4afeb33aa9f39d54259616df26c
Batuhan-Durhan <71403385+Batuhan-Durhan@users.noreply.github.com>
2022-03-12 01:18:40 +0000

    js

--- js/script.js
+++ js/script.js
@@ -8,9 +8,8 @@
  * Do basic math.
  */
 function doMathClicked() {
-  // this function does basic math
 
-  document.getElementedById('add-math').innerHTML = '<p>6 + 5 = ' (6 + 5) + '</p>'
+  document.getElementById('add-math').innerHTML = '<p>6 + 5 = ' + (6 + 5) + '</p>'
   document.getElementById('subtract-math').innerHTML = '<p>7 - 3 =  ' + (7 - 3) + '</p>'
   document.getElementById('multiply-math').innerHTML = '<p>3 + 4 × 2 = ' + (3 + 4 * 2) + '</p>'
   document.getElementById('divide-math').innerHTML = '<p>(4 ÷ 2) + 3 = ' + ((4 / 2) + 3) + '</p>'
